56edfe5a6a8e669fa7596d6ebd607fbedbdd13e4
jasonjosephwastaken <88890918+Jason-Joseph@users.noreply.github.com>
2021-09-21 19:01:32 +0700

    Add files via upload

---
+++ spyder/Practice Assignment 01.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 16:26:48 2021
+
+@author: jason
+"""
+#%%
+for i in range (1,11):
+    print(int(i))
+#%%
+for i in range (1,11):
+    print(i)
+#BEST ANSWER
+#%%   
+print(list(range(1,11)))
+    
+    
+
